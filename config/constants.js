b7fd6179d808a228030db8bc483fde3ecd8b019f
Silambarasan <simbu.bt@gmail.com>
2024-09-01 15:39:12 +0530

    #5 User list API completed

---
+++ config/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ERROR_CODES: {
+        BAD_REQUEST: 400,
+        UNAUTHORIZED: 401,
+        FORBIDDEN: 403,
+        NOT_FOUND: 404,
+        INTERNAL_SERVER_ERROR: 500
+    },
+    DEFAULT_ERROR_MESSAGE: "Some error occurred while retrieving data."
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,16 @@
 const User = require("../models/userModel.js");
+const Constants = require("../config/constants.js");
+
+// Retrieve all users from the database (with condition).
+exports.findAll = (req, res) => {
+    const username = req.query.username;
+  
+    User.getAll(username, (err, data) => {
+      if (err)
+        res.status(Constants.ERROR_CODES.INTERNAL_SERVER_ERROR).send({
+          message:
+            err.message || Constants.ERROR_CODES.DEFAULT_ERROR_MESSAGE
+        });
+      else res.send(data);
+    });
+  };
